7eabbce5c80a6d0d42df65ea4eeb07bc31dcca56
angular-cli <angular-cli@angular.io>
2015-11-08 01:13:50 +0800

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {AngularDemoZwApp} from './app/angular-demo-zw';
+
+
+bootstrap(AngularDemoZwApp);
---
+++ src/app/angular-demo-zw.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'angular-demo-zw-app',
+  providers: [],
+  templateUrl: 'app/angular-demo-zw.html',
+  directives: [],
+  pipes: []
+})
+export class AngularDemoZwApp {
+
+  constructor() {}
+
+}
