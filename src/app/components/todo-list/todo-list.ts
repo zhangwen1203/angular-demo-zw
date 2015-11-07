1e88a1f0ceabb0653b0d7f3387f0762ca994420e
Wen Zhang <zhangwen0330@gmail.com>
2015-11-08 01:24:03 +0800

    add todo list component

---
+++ src/app/components/todo-list/todo-list.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: 'app/components/todo-list/todo-list.html',
+  styleUrls: ['app/components/todo-list/todo-list.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class TodoList {
+
+  constructor() {}
+
+}
